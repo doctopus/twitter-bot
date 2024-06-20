8a371dccce954ac67e408fb1dcef377edc3a208d
Doctopus <tapasapt@gmail.com>
2024-06-20 08:21:01 -0400

    Removed magick and cowplot logic from R

--- components/magick-plotCoin.R
+++ components/magick-plotCoin.R
@@ -1,11 +1,23 @@
+# plotCoin.R
+
 # Load required libraries
 library(ggplot2)
 library(data.table)
+library(magick)
+library(cowplot)
 
 # Define directories
 input_dir <- dirname(normalizePath(commandArgs(trailingOnly = TRUE)[1]))
 output_dir <- dirname(normalizePath(commandArgs(trailingOnly = TRUE)[2]))
 input_file <- file.path(input_dir, "plotCoin.csv")
+
+# Read in the image file
+img <- magick::image_read(file.path(input_dir, "Canvas1500x500.png"))
+
+# Get image dimensions
+info <- image_info(img)
+img_width <- info$width
+img_height <- info$height
 
 # Read the data from the CSV file
 df <- fread(input_file)
@@ -27,5 +39,10 @@ p <- ggplot(df, aes(date, price)) +
         legend.background = element_rect(fill='transparent'),
         legend.box.background = element_rect(fill='transparent'))
 
-# Save the plot as a 1500x500 pixel image
-ggsave(file.path(output_dir, "Header1500x500.png"), p, dpi = 300, height = 500/300, width = 1500/300)
+# Combine image and plot
+plot <- ggdraw() +
+  draw_image(img) +
+  draw_plot(p)
+
+# Save the final plot
+ggsave(file.path(output_dir, "Header1500x500.png"), plot, dpi = 300, height = img_height, width = img_width, unit = 'px')
